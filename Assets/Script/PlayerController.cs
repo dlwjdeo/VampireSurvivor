fef001a7b6f4cf3c509884e0eededccea0bb4abe
dlwjdeo <87020463+dlwjdeo@users.noreply.github.com>
2025-07-03 11:22:59 +0900

    플레이어 애니메이션

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -9,10 +9,14 @@ public class PlayerController : MonoBehaviour
     public Vector2 inputVec;
     private Rigidbody2D _rigidbody;
     public float speed;
+    private SpriteRenderer _spriteRenderer;
+    private Animator _animator;
 
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _animator = GetComponent<Animator>();
     }
     
 
@@ -21,6 +25,15 @@ public class PlayerController : MonoBehaviour
         Vector2 nextVec = inputVec * speed * Time.fixedDeltaTime;
         _rigidbody.MovePosition(_rigidbody.position + nextVec);
         
+    }
+
+    private void LateUpdate()
+    {
+        _animator.SetFloat("Speed",inputVec.magnitude);
+        if (inputVec.x != 0)
+        {
+            _spriteRenderer.flipX = inputVec.x < 0;
+        }
     }
 
     private void OnMove(InputValue value)
